de8f8847309820db1d74dd9192007c61e10893b3
akagwyn <ivanghughes@gmail.com>
2022-08-30 17:00:38 -0300

    added if event

--- js/options.js
+++ js/options.js
@@ -1,5 +1,14 @@
 Chocolat(document.querySelectorAll('.chocolat-image'), {
     loop: true,
-    fullScreen: true,
+    // fullScreen: true,
+    allowZoom: false,
 
 })
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+    return document.addEventListener("DOMContentLoaded", function(event) { 
+        Chocolat(document.querySelectorAll('.chocolat-parent .chocolat-image'))
+    })
+  } else {
+    /* La pantalla tiene menos de 400 píxeles de ancho */
+  }

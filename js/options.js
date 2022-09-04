9dccf5c5bbc110a7b2f8b447bdcb4f26da9e00b2
akagwyn <ivanghughes@gmail.com>
2022-09-04 18:40:31 -0300

    more semantic

--- js/options.js
+++ js/options.js
@@ -1,16 +1,9 @@
-// Chocolat(document.querySelectorAll('.chocolat-image'), {
-//     loop: true,
-//     // fullScreen: true,
-//     allowZoom: false,
-
-// })
-
 let mql = window.matchMedia("(min-width: 768px)").matches;
-console.log(mql)
 
 function agregarGaleria() {
-    return document.addEventListener("DOMContentLoaded", function(event) { 
-        Chocolat(document.querySelectorAll('.chocolat-parent .chocolat-image'))
+    return document.addEventListener("DOMContentLoaded", function(event) {
+      Chocolat(document.querySelectorAll('.chocolat-parent .chocolat-image'),
+      {loop:true})
     })
 }
 
@@ -18,4 +11,14 @@ if (mql === true) {
     agregarGaleria()
   } else {
     null
-  }
+}
+
+console.log(mql)
+
+// img = document.getElementById("item");
+
+// img.onclick = function() {
+//   let img = document.getElementById("item");
+//   img.classList.add("makeFullScreen");
+// }
+
